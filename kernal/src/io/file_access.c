36f1fdfcc1be662baef1def42ca1fd083591a80d
usedRandomFunction <jamesmuir223@gmail.com>
2024-11-29 14:24:56 +0800

    Another small bug fix with rename
    forgot to set fat_last_sector to zero

--- kernal/src/io/file_access.c
+++ kernal/src/io/file_access.c
@@ -633,6 +633,7 @@ int rename(const char* old_path, const char* new_path)
         memcmp(old_path, new_path, dirrectorys_length) == 0)        // And be equal
     {
         // Since nothing is using the FAT we will use it working buffer here
+        last_fat_sector = 0;    // Set this to zero so a diffrent function knows this is not a part of the fat
         if (sd_write_section(directory_lba, file, directory_lba_offset * sizeof(fat_directory_entry), sizeof(fat_directory_entry), 1, fat_buffer) == 0)
         {
             free(file);     // Failed to write
@@ -683,6 +684,7 @@ int rename(const char* old_path, const char* new_path)
     memclr(file, sizeof(fat_directory_entry));  // We will use the file buffer as the space to write zeros into
 
     // Since nothing is using the FAT we will use it working buffer here
+    last_fat_sector = 0;    // Set this to zero so a diffrent function knows this is not a part of the fat
     if (sd_write_section(directory_lba, file, directory_lba_offset * sizeof(fat_directory_entry), sizeof(fat_directory_entry), 1, fat_buffer) == 0)
     {
         printf("Warning: File \"%s\" has been moved to \"%s\" but has failed to delete original entry, two copys pointing to same disk spce now exist!",
