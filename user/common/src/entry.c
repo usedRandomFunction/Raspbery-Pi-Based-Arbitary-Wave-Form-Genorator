36f12f0f785c632ce25720f17150e2bb71e6b652
usedRandomFunction <jamesmuir223@gmail.com>
2024-12-01 19:52:07 +0800

    Moved entry function to common

--- user/common/src/entry.c
+++ user/common/src/entry.c
@@ -6,13 +6,26 @@ int main();
 extern char __bss_start[]; // Linker will set this to point to the start of the program
 extern char __bss_end[]; // Linker will set this to point to the end of the program
 
-void entry()    // We still need to set up bss as it doesn't exist atm
+void _entry() __attribute__((section(".start")));
+
+void _create_bss();
+
+void _entry()
 {
-    size_t bss_size = (size_t)__bss_end - (size_t)__bss_start;
+    _create_bss();
+
+    exit(main());
+}
+
+void _create_bss()
+{
+    const size_t bss_size = (size_t)__bss_end - (size_t)__bss_start;
+
+    if (bss_size == 0)
+        return;
+
     if (vmemmap(&__bss_start, bss_size, VMEMMAP_WRITABILITY) < bss_size)
         exit(-9999);
         
     memclr(&__bss_start, bss_size);
-
-    exit(main());
 }
