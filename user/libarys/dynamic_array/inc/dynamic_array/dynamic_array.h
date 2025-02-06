0852c4032ccc94f783b33927d7fd88bd844ac97a
usedRandomFunction <jamesmuir223@gmail.com>
2025-02-06 15:48:47 +0800

    Added dynamic_array class to user libarys

---
+++ user/libarys/dynamic_array/inc/dynamic_array/dynamic_array.h
@@ -0,0 +1,67 @@
+#ifndef DYNAMIC_ARRAY_H
+#define DYNAMIC_ARRAY_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
+
+struct dynamic_array
+{
+    size_t number_of_entrys;
+    size_t size_of_entry;
+    void* ptr;
+};
+
+typedef struct dynamic_array dynamic_array;
+
+// Allocates and sets up the dynamic_array struct
+// @param size_of_entry The size of the entrys in bytes
+// @param n The number of entrys to allocate
+// @param header Pointer to the dynamic_array struct
+// @return false if failed to allocate buffer
+bool initialize_dynamic_array(size_t size_of_entry, size_t n, dynamic_array* header);
+
+// Allocates the array to be n + 1 entrys and inserts as one move
+// @param dynamic_array false if failed to allocate buffer
+bool insert_dynamic_array(void* entry, size_t index, dynamic_array* header);
+
+// Allocates the array to be new_size entrys
+// @param new_size The new size to set the array to
+// @param header Pointer to the dynamic_array struct
+// @return false if failed to allocate buffer
+bool resize_dynamic_array(size_t new_size, dynamic_array* header);
+
+// Allocates the array to be n - 1 entrys and copys the entry in such a way that removes the given entry
+// @param index The index to remove
+// @param header Pointer to the dynamic_array struct
+// @return false if failed to allocate buffer
+bool remove_dynamic_array_entry(size_t index, dynamic_array* header);
+
+// Deletes the array
+// @param header The array to delete
+void delete_dynamic_array(dynamic_array* header);
+
+
+typedef bool (*less_then_function)(const void*, const void*);
+typedef bool (*equal_to_function)(const void*, const void*);
+
+// Preforms a binary shearch on the given array but if failed gives the index it would be inserted into
+// @param header Pointer to the dynamic_array struct
+// @param entry The entry to shearch for
+// @param succss Pointer to bool, set to true if found, false if reutrning cloesest
+// @param less_then Returns true if A < B 
+// @param equal_to Returns true if A = B 
+// @return The index or (index to insert to) of entry
+size_t dynamic_array_find_closest_binary_shearch(const dynamic_array* header, const void* entry, bool* success, 
+    less_then_function less_then, equal_to_function equal_to);
+
+// Preforms a binary shearch on the given array
+// @param header Pointer to the dynamic_array struct
+// @param entry The entry to shearch for
+// @param less_then Returns true if A < B 
+// @param equal_to Returns true if A = B 
+// @return The index or -1 if failed
+size_t dynamic_array_binary_shearch(const dynamic_array* header, const void* entry, 
+    less_then_function less_then, equal_to_function equal_to);
+
+#endif
---
+++ user/libarys/dynamic_array/src/dynamic_array.c
@@ -0,0 +1,153 @@
+#include "dynamic_array.h"
+
+#include "common/memory.h"
+#include "common/alloc.h"
+#include "common/math.h"
+
+bool initialize_dynamic_array(size_t size_of_entry, size_t n, dynamic_array* header)
+{
+    header->number_of_entrys = n;
+    header->size_of_entry = size_of_entry;
+    header->ptr = NULL;
+
+    if (n == 0)
+        return true;
+
+    header->ptr = malloc(size_of_entry * n);
+
+    return header->ptr != NULL;
+}
+
+bool insert_dynamic_array(void* entry, size_t index, dynamic_array* header)
+{
+    void* new_buffer = malloc((header->number_of_entrys + 1) * header->size_of_entry);
+
+    if (new_buffer == NULL)
+        return false;
+
+    memcpy(new_buffer, header->ptr, index * header->size_of_entry);                     // Copy before the entry
+    memcpy(new_buffer + index * header->size_of_entry, entry, header->size_of_entry);   // Entry its self
+    memcpy(new_buffer + ((index + 1) * header->size_of_entry),                          // Index of entry after inserted
+        header->ptr + index * header->size_of_entry,                                    // Index of entry in old buffer at index
+        (header->number_of_entrys - index) * header->size_of_entry);                    // Number of bytes to copy
+
+    if (header->ptr != NULL)
+        free(header->ptr);
+
+    header->number_of_entrys++;
+    header->ptr = new_buffer;
+
+    return true;
+}
+
+bool resize_dynamic_array(size_t new_size, dynamic_array* header)
+{
+    void* new_buffer = NULL;
+    if (new_size != 0)
+    {
+        size_t size = min(header->number_of_entrys, new_size) * header->size_of_entry;
+
+
+        new_buffer = malloc(size);
+
+        if (new_buffer == NULL)
+            return false;
+
+        memcpy(new_buffer, header->ptr, size);
+    }
+    
+    if (header->ptr != NULL)
+        free(header->ptr);
+
+    header->number_of_entrys = new_size;
+    header->ptr = new_buffer;
+
+    return true;
+}
+
+bool remove_dynamic_array_entry(size_t index, dynamic_array* header)
+{   
+    void* new_buffer = NULL;
+    if (header->number_of_entrys > 1)
+    {
+        new_buffer = malloc((header->number_of_entrys - 1) * header->size_of_entry);
+
+        if (new_buffer == NULL)
+            return false;
+
+        memcpy(new_buffer, header->ptr, index * header->size_of_entry);                 // Copy before the entry
+        memcpy(((uint8_t*)new_buffer) + (index * header->size_of_entry),                // New offset of the next entry
+            ((uint8_t*)header->ptr) + ((index + 1) * header->size_of_entry),            // Offset of the next entry in src
+            (header->number_of_entrys - (index + 1)) * header->size_of_entry);          // Number of bytes left
+        free(header->ptr);
+    }
+    
+    header->number_of_entrys--;
+    header->ptr = new_buffer;
+
+    return true;
+}
+
+void delete_dynamic_array(dynamic_array* header)
+{
+    if (header->ptr != NULL)
+        free(header->ptr);
+}
+
+size_t dynamic_array_find_closest_binary_shearch(const dynamic_array* header, const void* entry, bool* success, 
+    less_then_function less_then, equal_to_function equal_to)
+{
+    size_t right = header->number_of_entrys - 1;
+    size_t left = 0;
+    *success = false;
+
+    if (header->number_of_entrys == 0)
+        return 0;
+
+    while (left < right)
+    {
+        size_t middle = left + right;
+        middle /= 2;
+
+        if (less_then((header->ptr + header->size_of_entry * middle), entry))
+        {
+            left = middle + 1;
+        }
+        else if (equal_to((header->ptr + header->size_of_entry * middle), entry))
+        {
+            *success = true;
+            return middle;
+        }
+        else
+        {
+            if (middle == 0)    // To stop roll over
+                break;
+
+            right = middle - 1;
+        }
+    }
+    
+    *success = equal_to((header->ptr + header->size_of_entry * left), entry);
+    
+    if (*success)
+        return left;
+
+    if (less_then((header->ptr + header->size_of_entry * left), entry))
+        return left + 1;
+
+    return left;
+}
+
+size_t dynamic_array_binary_shearch(const dynamic_array* header, const void* entry, 
+    less_then_function less_then, equal_to_function equal_to)
+{
+    bool success = false;
+
+    size_t index = dynamic_array_find_closest_binary_shearch(header,
+        entry, &success, less_then, equal_to);
+    
+    if (success)
+        return index;
+
+    return -1;
+}
